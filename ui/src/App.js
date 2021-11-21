904cccf2fb9fa598a5cf2a3394d4a1eadeca7b05
Jamie Sebastian <jamiesebastianjo@gmail.com>
2021-11-21 07:17:54 -0500

    route to login on default route

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,51 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
-import React, { Component } from 'react';
+import React, { Component, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const API_URL = 'http://localhost:8000/ping';
 
-class App extends Component  {
-  constructor(props) {
-    super(props);
+const App = () => {
+  const [message, setMessage] = React.useState('')
+  let navigate = useNavigate();
 
-    this.state = {
-      message: ""
-    };
-  }
+  useEffect(() => {
+    navigate("/login");
+  }, [])
 
-  componentDidMount() {
-    axios.get(API_URL).then(
-      response => {
-        console.log(response)
-        this.setState({
-          message: response.data.message
-        });
-      }
-    );
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-            {this.state.message}
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+          {message}
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
